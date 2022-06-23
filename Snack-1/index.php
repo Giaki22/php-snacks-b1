4da6ba5255ac045ed489086e9817ebc9b061eb47
Giaki22 <giaki22@hotmail.com>
2022-06-23 08:19:33 +0200

    first commit

---
+++ Snack-1/index.php
@@ -0,0 +1,43 @@
+<?php
+
+$games = [
+    [
+        'homeTeam' => 'Policastro',
+        'awayTeam' => 'Sapri',
+        'homeScore' => '70',
+        'awayScore' => '69'
+    ],
+    [
+        'homeTeam' => 'Bosco',
+        'awayTeam' => 'SGAP',
+        'homeScore' => '70',
+        'awayScore' => '75'
+    ],
+    [
+        'homeTeam' => 'CasalVelino',
+        'awayTeam' => 'Ascea',
+        'homeScore' => '60',
+        'awayScore' => '60'
+    ],
+    [
+        'homeTeam' => 'Vallo',
+        'awayTeam' => 'Maratea',
+        'homeScore' => '40',
+        'awayScore' => '50'
+    ],
+]
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+</head>
+<body>
+    
+</body>
+</html>
